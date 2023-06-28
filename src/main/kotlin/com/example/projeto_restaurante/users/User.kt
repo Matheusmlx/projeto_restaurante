e70c5706e86c93d2e7806fd1080bc88e1883af6d
matheus_pinho <matheus_pinho@econeteditora.com.br>
2023-06-28 13:03:33 -0300

    Add more methds in UserRepository

--- src/main/kotlin/com/example/projeto_restaurante/users/User.kt
+++ src/main/kotlin/com/example/projeto_restaurante/users/User.kt
@@ -1,6 +1,7 @@
 package com.example.projeto_restaurante.users
 
 class User(
+        var id: Long,
     var email: String,
     var password: String,
     var name: String
---
+++ src/main/kotlin/com/example/projeto_restaurante/users/userRepository.kt
@@ -0,0 +1,19 @@
+package com.example.projeto_restaurante.users
+
+import org.springframework.stereotype.Component
+
+@Component
+class userRepository {
+    val users = mutableListOf<User>();
+
+    fun save(user : User ): User {
+        user.id = (users.size + 1).toLong()
+        users.add(user)
+        return user
+    }
+    fun getById(userId: Long) = users.firstOrNull { user: User -> user.id == userId };
+
+    fun findAll(): List<User> = users.toList();
+
+    fun delete(userId: Long) = users.removeIf{ user: User -> user.id == userId  };
+}
