da03a0ebca7b25fd2ee77f9da3933d2a173f6d18
Matheusmlx <matheusluiz0303@outlook.com>
2023-07-09 22:54:36 -0300

    Sincronizaçao do JPA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id ("org.jetbrains.kotlin.plugin.allopen") version "1.9.0"
     id("org.springframework.boot") version "3.1.0"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
 }
 
 group = "com.example"
